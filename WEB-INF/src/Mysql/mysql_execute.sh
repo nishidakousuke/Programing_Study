80878d70fabc6aa0c08837cc7e2224217dc7dcf7
nishidakousuke <a-m-b.0228.roxsa.xion@ezweb.ne.jp>
2022-03-05 20:55:38 +0900

    second

--- WEB-INF/src/Mysql/mysql_execute.sh
+++ WEB-INF/src/Mysql/mysql_execute.sh
@@ -7,7 +7,7 @@ content=`cat ${mysql_dir}/mysql_code.txt`
 # 作れるテーブルの数を制限する
 TABLECREATECHECK=`echo ${content} | grep -i "create table" | wc -c`
 if [ ${TABLECREATECHECK} -gt 0 ]; then
-    TABLENUM=`mysql -h tomcat_test_mysql_1 -u root -prootpass programming -e "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'programming';" | sed -n 2p`
+    TABLENUM=`mysql -h tomcat_test_mysql_1 -u user -puserpass programming -e "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'programming';" | sed -n 2p`
     if [ ${TABLENUM} -ge 5 ]; then
         echo "You cannot create more than five tables." > ${mysql_dir}/mysql_result3.txt
         touch ${mysql_dir}/exists_flag.txt
@@ -16,21 +16,21 @@ if [ ${TABLECREATECHECK} -gt 0 ]; then
 fi
 
 # 入力されたsql構文を実行してファイルに保存する
-mysql -h tomcat_test_mysql_1 -u root -prootpass programming -e "${content}" >& ${mysql_dir}/mysql_result.txt
+mysql -h tomcat_test_mysql_1 -u user -puserpass programming -e "${content}" >& ${mysql_dir}/mysql_result.txt
 # column -t コマンドにより、出力結果を成形する。
 column -t ${mysql_dir}/mysql_result.txt > ${mysql_dir}/mysql_result2.txt
 # HTMLで表示させると改行がなくなるため、-zオプションにより\nを<br>に変換
 sed -z 's/\n/<br>/g' ${mysql_dir}/mysql_result2.txt > ${mysql_dir}/mysql_result3.txt
 
 #テーブル情報を表示させるための指示
-mysql -h tomcat_test_mysql_1 -u root -prootpass programming -e "select * from item;" >& ${mysql_dir}/table_show.txt
+mysql -h tomcat_test_mysql_1 -u user -puserpass programming -e "select * from item;" >& ${mysql_dir}/table_show.txt
 # column -t コマンドにより、出力結果を成形する。
 column -t ${mysql_dir}/table_show.txt > ${mysql_dir}/table_show2.txt
 # HTML表示用の改行をつける
 sed -z 's/\n/<br>/g' ${mysql_dir}/table_show2.txt > ${mysql_dir}/table_show3.txt
 
 #現在存在するテーブル名をファイルに保存する
-mysql -h tomcat_test_mysql_1 -u root -prootpass programming -e "show tables" >& ${mysql_dir}/table_contents.txt
+mysql -h tomcat_test_mysql_1 -u user -puserpass programming -e "show tables" >& ${mysql_dir}/table_contents.txt
 #1行目は不要な記述のため削除する
 sed '1d' ${mysql_dir}/table_contents.txt > ${mysql_dir}/table_contents2.txt
 
--- WEB-INF/src/Mysql/table_show.sh
+++ WEB-INF/src/Mysql/table_show.sh
@@ -4,7 +4,7 @@ mysql_dir="/usr/local/tomcat/webapps/Programing_Study/WEB-INF/src/Mysql"
 TABLENAME=$1
 
 #現在存在するテーブル名をファイルに保存する
-mysql -h tomcat_test_mysql_1 -u root -prootpass programming -e "show tables" >& ${mysql_dir}/table_contents.txt
+mysql -h tomcat_test_mysql_1 -u user -puserpass programming -e "show tables" >& ${mysql_dir}/table_contents.txt
 #1行目は不要な記述のため削除する
 sed '1d' ${mysql_dir}/table_contents.txt > ${mysql_dir}/table_contents2.txt
 
@@ -12,9 +12,9 @@ FRONTABLENAME=`head -n 1 ${mysql_dir}/table_contents2.txt`
 
 #テーブル情報を表示させるための指示。sql実行画面にリンクから飛んできた場合と、テーブル名をクリックして表示している場合とで分岐処理
 if test -z $TABLENAME; then
-  mysql -h tomcat_test_mysql_1 -u root -prootpass programming -e "select * from ${FRONTABLENAME};" >& ${mysql_dir}/table_show.txt
+  mysql -h tomcat_test_mysql_1 -u user -puserpass programming -e "select * from ${FRONTABLENAME};" >& ${mysql_dir}/table_show.txt
 else
-  mysql -h tomcat_test_mysql_1 -u root -prootpass programming -e "select * from ${TABLENAME};" >& ${mysql_dir}/table_show.txt
+  mysql -h tomcat_test_mysql_1 -u user -puserpass programming -e "select * from ${TABLENAME};" >& ${mysql_dir}/table_show.txt
 fi
 
 # column -t コマンドにより、出力結果を成形する。
